41e30ce13e06f95399952197d1a7705996e99c6d
Vincenzo Ninni <wampon@gmail.com>
2015-01-31 12:05:15 +0100

    Fix

--- paginator.js
+++ paginator.js
@@ -10,8 +10,8 @@ var Paginator = Backbone.View.extend({
 	initialize: function(params) {
 
 		this.current_page 			= 0;
-		this.page_size 				= params.page_size ? params.page_size : 10;
-		this.url 					= params.url;
+		this.page_size				= params.page_size ? params.page_size : 10;
+		this.url					= params.url;
 		this.data 					= params.data;
 		this.showDataCallBack 		= params.showDataCallBack;
 		this.paginator_container 	= params.paginator_container;
@@ -36,28 +36,30 @@ var Paginator = Backbone.View.extend({
 		me.data['limit'] = me.page_size;
 		
 		$.ajax({
-	        dataType: 'json',
-	        url: me.url,
-	        async: true,
-	        data: me.data,
+			dataType: 'json',
+			url: me.url,
+			async: true,
+			data: me.data,
 			
-	        success: function(data) {		
+			success: function(data) {		
 				if(data['results']!='' || me.current_page==0){
 					
 					me.showDataCallBack(data['results']);
 					me.render(data['founds']);
 				}
-				else{					
+				else{
 					me.current_page = me.current_page-1;
 					me.load();
 				}
-	        },
+			},
 			
 			error: function(data){
+
 				console.log(data);
+
 			}
 
-	    });
+		});
 
 	},
 
@@ -73,8 +75,7 @@ var Paginator = Backbone.View.extend({
 		
 		for (i = 0; i < pages; i++)
 
-			strHtml += 
-				'<li data-page="' + i + '"' + (i == this.current_page ? ' class="active" style="cursor:pointer;"' : '') + '><a href="#">' + (i + 1) + '</a></li>';
+			strHtml += '<li data-page="' + i + '"' + (i == this.current_page ? ' class="active" style="cursor:pointer;"' : '') + '><a href="#">' + (i + 1) + '</a></li>';
 
 		strHtml += '</ul>';
 
@@ -92,7 +93,7 @@ var Paginator = Backbone.View.extend({
 	*	Change page onclick non-active <li>
 	*/
 	changePage: function(event) {
-		
+
 		this.current_page = parseInt( $(event.currentTarget).attr("data-page") );
 		this.load();
 
